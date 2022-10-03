682ef823c8a2002c8413f306d25dd4a48c994d79
Kássia <kassia_guterres@outlook.com>
2022-10-03 14:20:03 -0300

    bug fix

--- src/routes/rentalsRoute.js
+++ src/routes/rentalsRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
-import {insertRentals, listRentals } from "../controllers/rentalsController.js";
+import {
+  insertRentals,
+  listRentals,
+  finalizingRental,
+} from "../controllers/rentalsController.js";
 import { rentalsValidation } from "../middlewares/rentalsMiddleware.js";
 
 const router = express.Router();
 router.get("/rentals", listRentals);
-router.post("/rentals",rentalsValidation, insertRentals)
+router.post("/rentals", rentalsValidation, insertRentals);
+router.post("/rentals/:id/return", finalizingRental );
 
 export default router;
